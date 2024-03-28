a57aba19d11e95ff96fc58999fc85dd7e4e7cdf2
Cameron Barnes <cameron_barnes@outlook.com>
2024-03-28 00:56:14 -0700

    minor formatting change

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,9 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use primes::sieve_eratosthenes1;
+use primes::sieve_eratosthenes;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("eratosthenes 100_000", |b| b.iter(|| black_box(sieve_eratosthenes1(1_301_789))));
+    c.bench_function("eratosthenes 100_000", |b| b.iter(|| black_box(sieve_eratosthenes(1_301_789))));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,25 @@ pub fn upper_bound_for_nth_prime(n: usize) -> usize {
     }
 }
 
+#[allow(
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::cast_possible_truncation
+)]
 #[must_use]
-pub fn sieve_eratosthenes1(bound: usize) -> Vec<usize> {
+pub fn lower_bound_for_nth_prime(n: usize) -> usize {
+    static LOW_PRIMES: [usize; 5] = [2, 3, 5, 7, 11];
+    if n < 6 {
+        LOW_PRIMES[n-1]
+    } else {
+        let n = n as f64;
+        let log_n = n.ln();
+        (n * (log_n + log_n.ln() - 1.)) as usize
+    }
+}
+
+#[must_use]
+pub fn sieve_eratosthenes(bound: usize) -> Vec<usize> {
     let n = bound;
     let mut is_prime = vec![true; n + 1];
     is_prime[0] = false;
@@ -48,6 +65,13 @@ pub fn sieve_eratosthenes1(bound: usize) -> Vec<usize> {
         .collect()
 }
 
+#[must_use]
+pub fn nth_prime(n: usize) -> usize {
+    let upper_bound = upper_bound_for_nth_prime(n);
+    let primes = sieve_eratosthenes(upper_bound);
+    primes[n - 1]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,15 +79,15 @@ mod tests {
 
     #[rstest]
     #[case(1, 2)]
-    #[case(10, 31)]
-    #[case(100, 613)]
-    #[case(1_000, 8_840)]
-    #[case(10_000, 104_921)]
-    #[case(100_000, 1_301_789)]
-    #[case(1_000_000, 15_502_802)]
-    #[case(10_000_000, 179_595_382)]
+    #[case(10, 29)]
+    #[case(100, 541)]
+    #[case(1_000, 7_919)]
+    #[case(10_000, 104_729)]
+    #[case(100_000, 1_299_709)]
+    #[case(1_000_000, 15_485_863)]
+    #[case(10_000_000, 179_424_673)]
     fn upper_bounds_testing(#[case] n: usize, #[case] expected: usize) {
-        assert_eq!(upper_bound_for_nth_prime(n), expected);
+        assert!(upper_bound_for_nth_prime(n) >= expected);
     }
 
     #[rstest]
@@ -75,8 +99,19 @@ mod tests {
     #[case(1_000_000, 15_485_863)]
     #[case(10_000_000, 179_424_673)]
     fn sieve_testing(#[case] n: usize, #[case] expected: usize) {
-        let upper_bound = upper_bound_for_nth_prime(n);
-        let primes = sieve_eratosthenes1(upper_bound);
-        assert_eq!(primes[n - 1], expected);
+        assert_eq!(nth_prime(n), expected);
+    }
+
+    #[rstest]
+    #[case(1, 2)]
+    #[case(10, 29)]
+    #[case(100, 541)]
+    #[case(1_000, 7_919)]
+    #[case(10_000, 104_729)]
+    #[case(100_000, 1_299_709)]
+    #[case(1_000_000, 15_485_863)]
+    #[case(10_000_000, 179_424_673)] 
+    fn lower_bounds_testing(#[case] n: usize, #[case] expected: usize) {
+        assert!(lower_bound_for_nth_prime(n) <= expected);
     }
 }
