423fd840fbd2286ef150d348557942c7c95408b1
Cameron Barnes <cameron_barnes@outlook.com>
2024-04-01 15:49:09 -0700

    implemented segmented/block sieve

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,10 +1,14 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use primes::sieve_eratosthenes;
+use primes::{block_sieve, nth_prime, sieve_eratosthenes};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("eratosthenes 100_000", |b| b.iter(|| black_box(sieve_eratosthenes(1_301_789))));
+    c.bench_function("sieve 10_001", |b| b.iter(|| nth_prime(black_box(10_001))));
+    c.bench_function("eratosthenes 100_000", |b| b.iter(|| sieve_eratosthenes(black_box(1_301_789))));
+    c.bench_function("eratosthenes 1_000_000", |b| b.iter(|| sieve_eratosthenes(black_box(179_595_382))));
+    c.bench_function("block 100_000", |b| b.iter(|| block_sieve(black_box(1_301_789))));
+    c.bench_function("block 1_000_000", |b| b.iter(|| block_sieve(black_box(179_595_382))));
 }
 
 criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+criterion_main!(benches); //179595382
--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ pub fn upper_bound_for_nth_prime(n: usize) -> usize {
 pub fn lower_bound_for_nth_prime(n: usize) -> usize {
     static LOW_PRIMES: [usize; 5] = [2, 3, 5, 7, 11];
     if n < 6 {
-        LOW_PRIMES[n-1]
+        LOW_PRIMES[n - 1]
     } else {
         let n = n as f64;
         let log_n = n.ln();
@@ -55,19 +55,90 @@ pub fn sieve_eratosthenes(bound: usize) -> Vec<usize> {
 
         i += 2;
     }
-    let mut primes : Vec<usize> = is_prime
+    let mut primes: Vec<usize> = is_prime
         .into_iter()
         .enumerate()
-        .filter_map(|(num, is_prime)| if is_prime { Some(num * 2 + 1) } else { None })
+        .filter_map(
+            |(num, is_prime)| {
+                let num = num * 2 + 1;
+                if is_prime && num <= bound {
+                    Some(num)
+                } else {
+                    None
+                }
+            },
+        )
         .collect();
     primes.insert(0, 2);
     primes
 }
 
+fn sieve_segment(primes: &[usize], lower_bound: usize, upper_bound: usize) -> Vec<usize> {
+    let mut is_prime = vec![true; upper_bound - lower_bound + 1];
+    for prime in primes {
+        let mut value = (prime * prime).max((lower_bound + prime - 1) / prime * prime);
+        while value <= upper_bound {
+            is_prime[value - lower_bound] = false;
+            value += prime;
+        }
+    }
+    if lower_bound == 1 {
+        is_prime[0] = false;
+    }
+    is_prime
+        .into_iter()
+        .enumerate()
+        .filter_map(
+            |(num, is_prime)| {
+                if is_prime {
+                    Some(num + lower_bound)
+                } else {
+                    None
+                }
+            },
+        )
+        .collect()
+}
+
+#[allow(
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::cast_possible_truncation
+)]
+#[must_use]
+pub fn block_sieve(bound: usize) -> Vec<usize> {
+    static BLOCK_SIZE: usize = 100_000;
+    if (bound as f64 * 0.9) as usize <= BLOCK_SIZE {
+                                                    // We're going to allow for the value to be
+                                                    // off by up to 10% and still use a simple
+                                                    // sieve, as at that size it shouldnt matter
+        return sieve_eratosthenes(bound);
+    }
+
+    let nsqurt = (bound as f64).sqrt() as usize;
+    let mut primes = if nsqurt <= BLOCK_SIZE {
+        sieve_eratosthenes(nsqurt)
+    } else {
+        block_sieve(nsqurt)
+    };
+
+    let mut completed = nsqurt;
+    let mut block_primes = Vec::new();
+    while completed < bound {
+        let upper_bound = (completed + BLOCK_SIZE).min(bound);
+        block_primes.append(&mut sieve_segment(&primes, completed + 1, upper_bound));
+        completed += BLOCK_SIZE;
+    }
+    primes.append(&mut block_primes);
+
+    primes
+}
+
+#[allow(clippy::cast_precision_loss, clippy::missing_panics_doc)]
 #[must_use]
 pub fn nth_prime(n: usize) -> usize {
     let upper_bound = upper_bound_for_nth_prime(n);
-    let primes = sieve_eratosthenes(upper_bound);
+    let primes = block_sieve(upper_bound);
     primes[n - 1]
 }
 
@@ -85,20 +156,8 @@ mod tests {
     #[case(100_000, 1_299_709)]
     #[case(1_000_000, 15_485_863)]
     #[case(10_000_000, 179_424_673)]
-    fn upper_bounds_testing(#[case] n: usize, #[case] expected: usize) {
-        assert!(upper_bound_for_nth_prime(n) >= expected);
-    }
-
-    #[rstest]
-    #[case(10, 29)]
-    #[case(100, 541)]
-    #[case(1_000, 7_919)]
-    #[case(10_000, 104_729)]
-    #[case(100_000, 1_299_709)]
-    #[case(1_000_000, 15_485_863)]
-    #[case(10_000_000, 179_424_673)]
-    fn sieve_testing(#[case] n: usize, #[case] expected: usize) {
-        assert_eq!(nth_prime(n), expected);
+    fn upper_bounds_testing(#[case] n: usize, #[case] prime: usize) {
+        assert!(upper_bound_for_nth_prime(n) >= prime);
     }
 
     #[rstest]
@@ -109,8 +168,52 @@ mod tests {
     #[case(10_000, 104_729)]
     #[case(100_000, 1_299_709)]
     #[case(1_000_000, 15_485_863)]
-    #[case(10_000_000, 179_424_673)] 
-    fn lower_bounds_testing(#[case] n: usize, #[case] expected: usize) {
-        assert!(lower_bound_for_nth_prime(n) <= expected);
+    #[case(10_000_000, 179_424_673)]
+    fn sieve_testing(#[case] n: usize, #[case] prime: usize) {
+        assert_eq!(nth_prime(n), prime);
+    }
+
+    #[rstest]
+    #[case(1, 2)]
+    #[case(10, 29)]
+    #[case(100, 541)]
+    #[case(1_000, 7_919)]
+    #[case(10_000, 104_729)]
+    #[case(100_000, 1_299_709)]
+    #[case(1_000_000, 15_485_863)]
+    #[case(10_000_000, 179_424_673)]
+    fn lower_bounds_testing(#[case] n: usize, #[case] prime: usize) {
+        assert!(lower_bound_for_nth_prime(n) <= prime);
+    }
+    
+    #[rstest]
+    #[case(1)]
+    #[case(10)]
+    #[case(100)]
+    #[case(1_000)]
+    #[case(10_000)]
+    #[case(100_000)]
+    #[case(1_000_000)]
+    #[case(10_000_000)]
+    fn method_test(#[case] n: usize) {
+        let bound = upper_bound_for_nth_prime(n);
+        assert_eq!(sieve_eratosthenes(bound), block_sieve(bound));
+    }
+ 
+    #[rstest]
+    #[case(100)]
+    #[case(1_300_000)]
+    fn sanity_check(#[case] n: usize) {
+        #[allow(
+            clippy::cast_precision_loss,
+            clippy::cast_sign_loss,
+            clippy::cast_possible_truncation
+        )]
+        let nsqurt = (n as f64).sqrt() as usize;
+        let mut primes = sieve_eratosthenes(nsqurt);
+        let mut segment = sieve_segment(&primes, nsqurt + 1, n);
+        primes.append(&mut segment);
+        let eratosthenes = sieve_eratosthenes(n);
+        assert_eq!(eratosthenes, primes);
     }
 }
